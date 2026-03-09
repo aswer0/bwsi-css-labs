6645ce0e27745a9bd4632ce01e37a3e561cfaf53
aswer0 <143581590+aswer0@users.noreply.github.com>
2026-03-08 19:15:53 -0700

    add personal intro to lab_1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Andrew" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! My name is Andrew Wong and I'm a junior from California. I became interested in robotics and engineering through doing FTC. I also enjoy working on several personal hands-on projects, such as 3d printed RC vehicles. In my free time, I like to play tennis and chess. I'm looking forward to learning a lot in this course!")
 
 if __name__ == "__main__":
     main()
